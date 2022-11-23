d659a1019f2b15a439f15b6c67317b43e89cc568
jacob <jacobdereje@gmail.com>
2022-11-22 18:02:32 -0600

    files

---
+++ src/linkedLists/restaraunts.java
@@ -0,0 +1,26 @@
+package linkedLists;
+
+import java.util.LinkedList;
+
+
+public class restaraunts {
+
+    public static void main(String[] args) {
+        LinkedList<String> restaraunts = new LinkedList<String>();
+
+        restaraunts.push("Chick Fil a");
+
+        restaraunts.push("Popeyes");
+
+        restaraunts.push("Olive Garden");
+
+        restaraunts.add(2, "Canes");
+
+        System.out.println(restaraunts);
+
+        restaraunts current = head;
+
+    }
+
+
+}
